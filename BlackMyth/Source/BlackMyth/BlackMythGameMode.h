46dfb6e680a929580da2e94dc9af1a9ed554c799
wendaining <wendaining@foxmail.com>
2025-12-02 15:29:12 +0800

    [Feat] 新建了游戏项目

---
+++ BlackMyth/Source/BlackMyth/BlackMythGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackMythGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABlackMythGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABlackMythGameMode();
+};
+
+
+
