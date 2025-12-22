dfeabd66f23328e9ec9091fe7c9b9a69849e3bc5
onnisama <3204096434@qq.com>
2025-12-22 20:10:55 +0800

    [fix] 修复了怪物在新地图刷新错误的问题，并经检验存档读档有效

--- BlackMyth/Source/BlackMyth/EnemySpawner.cpp
+++ BlackMyth/Source/BlackMyth/EnemySpawner.cpp
@@ -27,18 +27,40 @@ void AEnemySpawner::BeginPlay()
 
 AEnemyBase* AEnemySpawner::SpawnEnemy(TSubclassOf<AEnemyBase> EnemyClass, const FVector& Location, const FRotator& Rotation, int32 Level)
 {
-    if (!EnemyClass) return nullptr;
+
+    if (!EnemyClass)
+    {
+        UE_LOG(LogTemp, Error, TEXT("SpawnEnemy failed: EnemyClass is NULL"));
+        return nullptr;
+    }
+
+    UE_LOG(LogTemp, Warning, TEXT("Spawning Enemy at %s"), *Location.ToString());
 
     FActorSpawnParameters SpawnParams;
     SpawnParams.Owner = this;
+    SpawnParams.SpawnCollisionHandlingOverride =
+        ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
     AEnemyBase* SpawnedEnemy = GetWorld()->SpawnActor<AEnemyBase>(EnemyClass, Location, Rotation, SpawnParams);
 
     if (SpawnedEnemy)
     {
-        SpawnedEnemy->SpawnerName = GetName(); // 设置Spawner名称
+        // 抬高一点，防止卡地
+        FVector SafeLocation = SpawnedEnemy->GetActorLocation();
+        SafeLocation.Z += 100.f;
+
+        SpawnedEnemy->SetActorLocation(
+            SafeLocation,
+            false,
+            nullptr,
+            ETeleportType::TeleportPhysics
+        );
+
+        SpawnedEnemy->SpawnerName = GetName();
         SpawnedEnemy->InitEnemy(Level, true);
         SpawnedEnemies.Add(SpawnedEnemy);
     }
+
 
     return SpawnedEnemy;
 }
